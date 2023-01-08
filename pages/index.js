c78fe2d91eea322ca81dae61c11c8f5bc6dcbf4c
ElmerPC <erey2790@gmail.com>
2023-01-08 17:17:10 -0600

    added structure to main index

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,32 @@ export default function Home() {
         </nav>
       </header>
       <main className={styles.main}>
-       
+       <a href=''> 
+        <img />
+        <h2> Events in London</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+       </a>
+
+       <a href=''> 
+        <img />
+        <h2> Events in San Francisco</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+       </a>
+
+       <a href=''> 
+        <img />
+        <h2> Events in London</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+       </a>
       </main>
+
+      
 
       <footer className={styles.footer}>
         <p> © 2022 Time to Code - A Project with Next.JS</p>
