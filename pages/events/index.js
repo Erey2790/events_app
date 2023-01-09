f9d7808aefb8488299f8aa3f1cf4d75bb954ea6a
ElmerPC <erey2790@gmail.com>
2023-01-08 18:19:58 -0600

    created my first dynamic file in nextjs

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,27 @@
 const Page = () => {
     return (
+        <div>
         <h1>Events Page</h1>
+            <div>
+            <a href=''> 
+        <img />
+        <h2> Events in London</h2>
+        
+       </a>
+
+       <a href=''> 
+        <img />
+        <h2> Events in San Francisco</h2>
+        
+       </a>
+
+       <a href=''> 
+        <img />
+        <h2> Events in London</h2>
+        
+       </a>
+            </div>
+        </div>
     )
 }
 
