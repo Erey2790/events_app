3f1a1369c9148e16ac4e44bf79d43cc20e78a1a1
ElmerPC <erey2790@gmail.com>
2023-01-09 22:52:19 -0600

    created the staticPaths func

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,16 +1,54 @@
 const EventsCatPage = () => {
-    return (
-        <div>
-        <h1>Events in London</h1>
+  return (
+    <div>
+      <h1>Events in London</h1>
+      <div>
+        <a href="/events/london/ev1">
+            <img /> <h2> Event 1 </h2>
+        </a>
+        <a href="">
+            <img /> <h2> Event 2 </h2>
+        </a>
+        <a href="">
+            <img /> <h2> Event 3 </h2>
+        </a>
+        <a href="">
+            <img /> <h2> Event 4 </h2>
+        </a>
+        <a href="">
+            <img /> <h2> Event 5 </h2>
+        </a>
+        <a href="">
+            <img /> <h2> Event 6 </h2>
+        </a>
 
-        <a href='/events/event1'> Event 1</a>
-        <a href="/events/event2"> Event 2</a>
-        <a href="/events/event3"> Event 3</a>
-        <a href="/events/event4"> Event 4</a>
-        <a href="/events/event5"> Event 5</a>
-        <a href="/events/event6"> Event 6</a>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default EventsCatPage;
+
+export async function getStaticPaths() {
+    const { events_categories } = await import('/data/data.json')
+    const allPaths = events_categories.map((ev) => {
+        return {
+            params: {
+                cat: ev.id.toString(),
+            }
+        }
+    });
+    console.log(allPaths)
+    return {
+        paths: allPaths,
+        fallback: false
+
+    }
+}
+
+export async function getStaticProps(context) {
+    console.log(context);
+    return {
+        props: {}
+    }
+}
