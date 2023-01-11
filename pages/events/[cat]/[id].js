b4c65522db9649ca02aa0be662412760f25165f4
ElmerPC <erey2790@gmail.com>
2023-01-11 16:10:48 -0600

    props and data added to single page

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,6 +1,13 @@
-const EventPage = () => {
+import Image from "next/image";
+
+const EventPage = ( {data} ) => {
+    console.log(data)
     return (
-        <h1>Our Single Event</h1>
+        <div>
+            <Image src={data.image} width={800} height={500} alt={data.title} />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
     )
 }
 
@@ -27,7 +34,11 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     console.log(context)
+    const { allEvents } = await import('/data/data.json');
+    const id = context?.params.id;
+
+    const eventData = allEvents.find((ev) => ev.id === id);
     return {
-        props: {}
+        props: { data: eventData}
     }
 }
