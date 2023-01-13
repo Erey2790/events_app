c4f1185c9b2e57e562bbd9353f8f0844d34d9aa5
ElmerPC <erey2790@gmail.com>
2023-01-13 16:38:12 -0600

    found sass err

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,43 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <header>
-      <div>
+      <div className="topNav">
+        <Image
+          alt="logo"
+          src={"/images/logo_black.jpg"}
+          width={50}
+          height={50}
+        />
         <nav>
-          <img />
-          <Link href="/" legacyBehavior>
-            <a> Home</a>
-          </Link>
-          <Link href="/events" legacyBehavior>
-            <a> Events</a>
-          </Link>
-          <Link href="/about-us" legacyBehavior>
-            <a> About Us</a>
-          </Link>
+          <ul>
+            <li>
+              <Link href="/" legacyBehavior>
+                <a> Home</a>
+              </Link>
+            </li>
+          
+
+          
+            <li>
+              <Link href="/events" legacyBehavior>
+                <a> Events</a>
+              </Link>
+            </li>
+          
+
+          
+            <li>
+              <Link href="/about-us" legacyBehavior>
+                <a> About Us</a>
+              </Link>
+            </li>
+          </ul>
         </nav>
       </div>
+      <h1> Sed ut perspiciatis unde omnis</h1>
     </header>
   );
 };
